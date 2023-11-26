ae2d119089015408742520e76d562490a50d4f91
Edgar Haro <harove@gmail.com>
2023-11-25 23:13:10 -0300

    render products with handlebars and prep realtime

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import { apiRouter } from './routers/api.router.js'
-// import {  } from './midlewares/midlewares.js'
+import handlebars from 'express-handlebars'
+import { webRouter } from './routers/web.Router.js'
+import {Server} from 'socket.io'
 
+// import {  } from './midlewares/midlewares.js'
 
 const app = express()
 
+//motor de plantillas
+app.engine('handlebars', handlebars.engine())
+app.set('views', './views')
+
+//middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
@@ -12,9 +20,18 @@ app.use(express.static('./public'))
 app.use(express.static('./views'))
 app.use('/static', express.static('./static'))
 
-app.use(apiRouter)
+const server = app.listen(8080, ()=> {console.log('conectado')})
+
+const websocketServer = new Server(server)
+
+//Routers
+app.use('/api',apiRouter)
+app.use('/',webRouter)
 app.use((req, res, next) => {
     res.status(404).send('Not Found');
 });
 
-app.listen(8080, ()=> {console.log('conectado')})
+
+websocketServer.on('connection', (socket)=>{
+    console.log(socket.id)
+})
--- src/routers/api.router.js
+++ src/routers/api.router.js
@@ -4,5 +4,5 @@ import { cartsRouter } from './carts.router.js';
 
 
 export const apiRouter = Router()
-apiRouter.use('/api/products', productsRouter)
-apiRouter.use('/api/carts', cartsRouter)
+apiRouter.use('/products', productsRouter)
+apiRouter.use('/carts', cartsRouter)
